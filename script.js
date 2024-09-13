c66439b063d77389080e82b6fc7aff55edaedd22
MaryMaistrova <maistrovamariia@gmail.com>
2024-09-13 20:21:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let name = 'MaryMaistrova'
+const fullName = 'MariiaM'
+let Name = "Mary", LastName = 'Maistrova'
+let $ = 'namesurname'
+let _me = 'myfullname'
+
+// wrong names
+// let 123 = 'myname'
+// const my-name = 'manameagain'
+// let let = 'fullname'
+
+// way of commenting - ctrl+/
+/*way of commenting - multi-lines
+way of commenting*/
+
+
+// стилі написання змінних
+// 1. Довго і нудно
+// let message;
+// message = 'Привіт!';
+
+// 2. Короткий
+// let message = 'Привіт!';
+
+// 3. Декілька змінних разом
+// let user = 'Іван', age = 25, message = 'Привіт';
+
+// 4. Багаторядковий спосіб
+// let user = 'Іван';
+// let age = 25;
+// let message = 'Привіт';
+
+// 5. Інший запис
+// let user = 'Іван',
+//   age = 25,
+//   message = 'Привіт';
